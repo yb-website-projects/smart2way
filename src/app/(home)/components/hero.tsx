612c5797f79a0aa98dc26aaa6a3e341e6810c5e5
Daniil <dan.developer@born2be.media>
2025-06-16 18:33:58 +0300

    [feature/logo-favicon]: Logo & Favicon (#6)

--- src/app/(home)/components/hero.tsx
+++ src/app/(home)/components/hero.tsx
@@ -38,17 +38,19 @@ export const Hero = () => {
 };
 
 const HeroTitle = () => (
-  <FadeIn
-    variant="bottom"
-    className="absolute top-[40%] left-1/2 z-20 flex w-[70%] -translate-x-1/2 -translate-y-1/2 flex-col gap-4 text-center max-md:w-[90%]"
-  >
-    <Text color="primary" uppercase>
-      Your Personalized Route to Global Trading Access
-    </Text>
-    <Title>Step into Wiser Trading with Smart2Way</Title>
-    <Text color="secondary">
-      We assist you in finding a broker that aligns with your unique preferences
-      and strategies.
-    </Text>
-  </FadeIn>
+  <section className="absolute top-[30%] left-1/2 z-20 flex w-[70%] -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-[100px] text-center max-md:w-[90%]">
+    <FadeIn>
+      <Image src="/full-logo-white.svg" alt="logo" width={185} height={40} />
+    </FadeIn>
+    <FadeIn variant="bottom" className="flex flex-col gap-4 text-center">
+      <Text color="primary" uppercase>
+        Your Personalized Route to Global Trading Access
+      </Text>
+      <Title>Step into Wiser Trading with Smart2Way</Title>
+      <Text color="secondary">
+        We assist you in finding a broker that aligns with your unique
+        preferences and strategies.
+      </Text>
+    </FadeIn>
+  </section>
 );
---
+++ src/shared/ui/components/logo.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import Image from 'next/image';
+
+export const Logo = () => (
+  <Image src="/full-logo-white.svg" alt="logo" width={185} height={40} />
+);
