8af8405a6ceb0eed5438c0ae43a13263ca8bbf3c
Daniil <dan.developer@born2be.media>
2025-06-24 15:52:33 +0300

    [feature/meta]: Meta (#15)

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,5 @@
+import type { Metadata } from 'next';
+
 import {
   Benefits,
   ContactUs,
@@ -7,6 +9,23 @@ import {
   WeComitted,
   WeSupport,
 } from './components';
+
+export const metadata: Metadata = {
+  title: 'Smart2Way | Find the Right Broker for Your Trading Strategy',
+  description:
+    'Smart2Way helps traders discover the most suitable brokers worldwide. Save time, reduce costs, and access expert guidance to optimize your trading journey with our broker search solutions.',
+  openGraph: {
+    title: 'Smart2Way | Find the Right Broker for Your Trading Strategy',
+    description:
+      'Smart2Way helps traders discover the most suitable brokers worldwide. Save time, reduce costs, and access expert guidance to optimize your trading journey with our broker search solutions.',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Smart2Way | Find the Right Broker for Your Trading Strategy',
+    description:
+      'Smart2Way helps traders discover the most suitable brokers worldwide. Save time, reduce costs, and access expert guidance to optimize your trading journey with our broker search solutions.',
+  },
+};
 
 export default function Home() {
   return (
